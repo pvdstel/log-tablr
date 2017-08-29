901070aa6d26742a6cc29070dea871881e8c9bbc
Paul <pvdstel@users.noreply.github.com>
2017-08-29 23:15:36 +0200

    Add Cycle Profile to Agent Profiles

--- src/components/Data/AgentProfile.tsx
+++ src/components/Data/AgentProfile.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { AgentProfile as AgentProfileDataType } from 'src/structures/exportFormat';
 import ModuleProfiles from './ModuleProfiles';
+import CycleProfile from './CycleProfile';
 
 export interface IModuleProfilesProps {
     agentProfile: AgentProfileDataType;
@@ -9,6 +10,7 @@ export interface IModuleProfilesProps {
 
 interface IModuleProfilesState {
     agentProfile: AgentProfileDataType;
+    showCycleProfile: boolean;
 }
 
 export default class AgentProfile extends React.Component<IModuleProfilesProps, IModuleProfilesState> {
@@ -16,8 +18,14 @@ export default class AgentProfile extends React.Component<IModuleProfilesProps, 
         super(props);
 
         this.state = {
-            agentProfile: props.agentProfile
+            agentProfile: props.agentProfile,
+            showCycleProfile: false
         };
+    }
+
+    private onToggleCycleProfileClicked = (e: React.SyntheticEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        this.setState({ showCycleProfile: !this.state.showCycleProfile });
     }
 
     componentWillReceiveProps(nextProps: IModuleProfilesProps) {
@@ -30,8 +38,20 @@ export default class AgentProfile extends React.Component<IModuleProfilesProps, 
         return (
             <section>
                 <h4 className='title is-4 has-text-grey'>{this.state.agentProfile.Name}</h4>
+
                 <h5 className='title is-5 has-text-grey-light'>Module profiles</h5>
                 <ModuleProfiles moduleProfiles={this.state.agentProfile.ModuleProfiles} />
+
+                <h5 className='title is-5 has-text-grey-light'>Cycle profile</h5>
+                <p className='bottom-p-spacing'>
+                    <a onClick={this.onToggleCycleProfileClicked}
+                        href='#'>
+                        Show/hide cycle profile
+                    </a>
+                </p>
+                {this.state.showCycleProfile &&
+                    <CycleProfile cycleProfile={this.state.agentProfile.CycleProfile} />
+                }
             </section>
         );
     }
