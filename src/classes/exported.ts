72bec20a494d9c4e67bc8f10ce8d897be8368d96
Paul <pvdstel@users.noreply.github.com>
2017-08-29 21:26:01 +0200

    Add data interfaces

---
+++ src/classes/exported.ts
@@ -0,0 +1,55 @@
+export interface ExportFormat {
+    Name: string;
+    ModuleProfiles: ModuleProfile[];
+    AgentProfile: AgentProfile[];
+    CycleProfile: CycleProfile;
+}
+
+export interface ModuleProfile {
+    Name: string;
+    Executions: number;
+    TotalExecutionTime: number;
+    ShortestExecutionTime: number;
+    LongestExecutionTime: number;
+    AverageExecutionTime: number;
+}
+
+export interface AgentProfile {
+    Name: string;
+    ModuleProfiles: ModuleProfile[];
+    CycleProfile: CycleProfile;
+}
+
+export interface CycleProfile {
+    Count: number;
+    TotalTime: number;
+    AverageTime: number;
+    LowestTime: number;
+    HighestTime: number;
+    TotalActions: number;
+    AverageActions: number;
+    LowestActions: number;
+    HighestActions: number;
+    TotalBeliefs: number;
+    AverageBeliefs: number;
+    LowestBeliefs: number;
+    HighestBeliefs: number;
+    TotalQueries: number;
+    HighestQueries: number;
+    TotalPercepts: number;
+    AveragePercepts: number;
+    LowestPercepts: number;
+    HighestPercepts: number;
+    TotalGoals: number;
+    AverageGOals: number;
+    LowestGoals: number;
+    HighestGoals: number;
+    TotalMessagesReceived: number;
+    AverageMessagesReceived: number;
+    LowestMessagesReceived: number;
+    HighestMessagesReceived: number;
+    TotalMessagesSent: number;
+    AverageMessagesSent: number;
+    LowestMessagesSent: number;
+    HighestMessagesSent: number;
+}
