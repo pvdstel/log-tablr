da3b9e7d6b750b9e341b014a37a38c8e56f6d060
Paul <pvdstel@users.noreply.github.com>
2017-08-29 20:36:26 +0200

    Basic project setup

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <p>Test</p>
+    );
+}
+
+const root = document.querySelector('#app');
+ReactDOM.render(<App />, root);
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var path = require('path');
+
+const cssLoaderConfig = {
+  sourceMap: true
+};
+
+module.exports = {
+  entry: './src/index.tsx',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/assets'),
+    publicPath: '/assets/'
+  },
+
+  devtool: 'source-map',
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    contentBase: './dist',
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+
+  resolve: {
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+      '.jsx',
+      '.css',
+      '.scss'
+    ],
+    modules: [
+      path.resolve(__dirname),
+      path.resolve(__dirname, 'node_modules')
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: [
+          'awesome-typescript-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: cssLoaderConfig
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: cssLoaderConfig
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.svg$|\.png$|\.jpe?g$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]-[hash:base64:5].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
