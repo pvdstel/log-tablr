1f8fa958c994e25060e7e869d57c599f3fead8fd
Paul <pvdstel@users.noreply.github.com>
2017-08-29 21:36:47 +0200

    Add JSON parsing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { ExportFormat } from './structures/exportFormat';
 import Header from './components/header';
 
 document.body.dataset.appLoaded = '';
@@ -8,13 +9,18 @@ document.body.dataset.appLoaded = '';
 require('bulma/bulma.sass');
 
 function App() {
+    let data: ExportFormat, dataContainer = document.getElementById('data-container');
+    if (dataContainer) {
+        data = JSON.parse(dataContainer.innerHTML);
+    }
+
     return (
         <div>
             <Header />
             <hr />
             <section className='section'>
                 <div className='container'>
-                    <p>Test</p>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
                 </div>
             </section>
         </div>
